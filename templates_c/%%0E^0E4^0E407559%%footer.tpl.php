1190221f7649bbbbb373f726ba78985bde1b7791
irfan907 <75222952+irfan907@users.noreply.github.com>
2021-06-11 15:27:55 +0500

    Added Office and Store Menu

--- templates_c/%%0E^0E4^0E407559%%footer.tpl.php
+++ templates_c/%%0E^0E4^0E407559%%footer.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.18, created on 2021-06-11 15:52:16
+<?php /* Smarty version 2.6.18, created on 2021-06-11 18:27:22
          compiled from footer.tpl */ ?>
 <?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
 smarty_core_load_plugins(array('plugins' => array(array('modifier', 'date_format', 'footer.tpl', 24, false),)), $this); ?>
